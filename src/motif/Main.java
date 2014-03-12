19d26e207deb0c11acffcd93b0ab202b38e3a3f0
Spkordell <mask@mask>
2014-03-11 21:04:10 -0400

    Added sequential pattern mining library

---
+++ src/motif/Main.java
@@ -0,0 +1,56 @@
+package motif;
+
+import java.awt.*;
+import java.util.LinkedList;
+
+import javax.swing.*;
+
+
+public class Main {
+	private static JFrame frame;
+	private static JPanel mainPanel;
+	private static LinkedList<Component> cList;
+	
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+    	cList = new LinkedList<Component>();
+    	mainPanel = new JPanel(new GridLayout(1, 1));    	
+        //Create and set up the window.
+        frame = new JFrame("Ipsum");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(mainPanel,BorderLayout.CENTER);      
+           
+        
+        //Display the window.
+        frame.setPreferredSize(new Dimension(700,700));
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+	public static JPanel getMainPanel() {
+		return mainPanel;
+	}
+	
+	public static void add(Component component) {
+		cList.add(component);
+		getMainPanel().setLayout(new GridLayout(cList.size(), 1));
+		for(Component comp : cList) {
+			getMainPanel().add(comp);
+		}
+	}
+}
