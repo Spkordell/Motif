ab9d93fa73369ef78a33165862982f7354281e38
Spkordell <mask@mask>
2014-05-30 23:14:40 -0400

    Began refactoring into class structure

--- src/motif/Main.java
+++ src/motif/Main.java
@@ -2,11 +2,11 @@ package motif;
 
 import java.awt.*;
 import java.util.LinkedList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Stack;
 
 import javax.swing.*;
 
+import sun.misc.Queue;
 
 public class Main {
 	//private static JFrame frame;
@@ -26,167 +26,33 @@ public class Main {
         //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //frame.setLayout(new BorderLayout());
         //frame.add(mainPanel,BorderLayout.CENTER);      
-           
-        
+
         //Display the window.
         //frame.setPreferredSize(new Dimension(700,700));
         //frame.pack();
         //frame.setVisible(true);
-        
-        //String arr[] = {"1 2 3 4 1 2 3 4 1 2 3 4 ", "1 2 1 2 1 2 1 2 ", "1 1 2 1 1 3 1 1 2 1 1 3 1 1 2 ", "111 110 2 112 111 110 2 112 111 "};
-        String arr[] = {"1 2 1 2 1 3 1 2 1 2 1 3 1 "};
-        LinkedList<String> patterns;
-        //LinkedList<Float> patternStrengths;
-        for (String elem : arr) {
-        	patterns = new LinkedList<String>();
-        	//patternStrengths = new LinkedList<Float>();
-	        System.out.println("------"+elem+"------");
-	        for (int i = 2; i < elem.length()/2; i+=1){
-        	   String regex = "((\\d+\\s){"+i+"}).*\\1";
-		       Pattern p = Pattern.compile(regex);
-	           Matcher matcher = p.matcher(elem);
-	           if (matcher.find()) {
-	        	  patterns.add(matcher.group(1).trim());
-	        	  //patternStrengths.add(determinePatternStrength(elem,matcher.group(1).trim()));
-	        	  int j = 0;
-	        	  do {
-		        	  j = elem.indexOf(' ',++j);
-	        		  if (matcher.find(j) && !patterns.contains(matcher.group(1).trim())) {
-	   		       		  patterns.add(matcher.group(1).trim());
-	   		       		  //patternStrengths.add(determinePatternStrength(elem,matcher.group(1).trim()));
-	   		       	  }        	  
-	        	  } while (j < elem.length()-i); //todo: this can probably end sooner
-	           } else {
-	        	  //we won't find anymore patterns, leave loop early
-	           	  break;
-	           }
-	       }
-      	   //we're done, print all the patterns we found
-     	   int index = 0;
-     	   for(String pattern : patterns){
-     		   System.out.println("Pattern "+(index++)+": "+pattern/*+" = "+patternStrengths.get(index-1)*100+"%"*/);
-     	   }
-     	   
-        	//make a prediction as to what might come next	 
-        	String regex;
-        	Pattern p;
-        	Matcher matcher;
-        	LinkedList<String> predictions = new LinkedList<String>();
-        	LinkedList<Float> predictionStrengths = new LinkedList<Float>();
-        	for(String pattern : patterns) {
-        		regex = "\\s(";
-        		int i = 0;
-        		while ( (i = pattern.indexOf(' ',++i)) != -1) {
-        			regex+=pattern.substring(0,i).trim()+"|";
-        		}
-        		if (regex.endsWith("|")) {
-        			regex = regex.substring(0,regex.length()-1);
-        		}
-        		regex += ")\\s$";
-        		//System.out.println(regex);
-        		p = Pattern.compile(regex);        	        		
-        		matcher = p.matcher(elem);
-        		if (matcher.find()) {
-        			//This pattern matches some part of the end of the input
- 
-        			//chop off the portion of the pattern that intersects with the end of the input, leaving just the prediction
-        			int idx = pattern.length();
-                 	while (!elem.endsWith(pattern.substring(0, idx--)));
-                 	String prediction = pattern.substring(idx + 1);
-                 	
-                 	Float predictionStrength = determinePredictionStrength(elem, pattern, prediction);
-                 	
-                 	if (!predictions.contains(prediction)) { //remove repeated predictions
-              	   		predictions.add(prediction);
-              	   		predictionStrengths.add(predictionStrength);//The prediction strength is equal to the strength of the pattern the prediction came from
-                 	} else {
-                 		//compare strengths and keep the largest
-                 		int predictionIndex = predictions.indexOf(prediction);
-                 		if (predictionStrengths.get(predictionIndex) <  predictionStrength) {
-                 			predictionStrengths.set(predictionIndex, predictionStrength);
-                 		}
-                 	}
-        		}
-        	}
-        	
-    		float largestPredictionStrength = -1;
-    		
-        	if (predictions.size() == 0) {
-        		System.out.println("No predicions");
-        	} else {
-        		int predictionIndex = 0;
-	      	   	for(String prediction : predictions){
-	      	   		System.out.println("Prediction: "+prediction+" = "+predictionStrengths.get(predictionIndex)*100+"%");
-	      	   		if (predictionStrengths.get(predictionIndex) > largestPredictionStrength) {
-	      	   			largestPredictionStrength = predictionStrengths.get(predictionIndex);
-	      	   		}
-	      	   		predictionIndex++;
-	      	   	}
-        	}
-        	
-        	//Select a prediction (first, find prediction with highest accuracy. Break ties by selecting prediction with most elements.
-          	if (predictions.size() > 0) {
-          		int predictionIndex = 0;
-          		int largestPredictionLength = -1;
-          		String selectedPrediction = null;
-          		for(String prediction : predictions){
-          			int predictionLength = countElements(prediction);
-	      	   		if (predictionStrengths.get(predictionIndex) == largestPredictionStrength && predictionLength > largestPredictionLength) {
-	      	   			selectedPrediction = prediction;
-	      	   			largestPredictionLength = predictionLength;
-	      	   		}
-	      	   		predictionIndex++;
-	      	   	}
-          		System.out.println("Selected Prediction: "+selectedPrediction+" = "+largestPredictionStrength*100 + "%");
-        	}
-        }
+    	
+    	Node firstNode = new Node();
+    	firstNode.step(1);
+    	firstNode.step(2);
+    	firstNode.step(1);
+    	firstNode.step(2);
+    	firstNode.step(1);
+    	firstNode.step(3);
+    	firstNode.step(1);
+    	firstNode.step(3);
+    	firstNode.step(1);
+    	firstNode.step(2);
+    	firstNode.step(1);
+    	firstNode.step(2);
+    	firstNode.step(1);
+    	firstNode.step(3);
+    	firstNode.step(1);
+    	firstNode.step(3);
+    	firstNode.step(1);
+
     }
-
     
-    private static int countElements(String string) {
-		return string.split(" ").length;
-	}
-
-
-	private static float determinePredictionStrength(String elem, String pattern, String prediction) {
-    	//count number of times pattern appears
-    	int patternCount = elem.split("(?="+pattern+")").length-1; //The lookahead allows the split to capture overlaps
-    	
-    	//count number of times prediction would have been wrong	 
-     	int patternMissedCount = (elem.split("(?="+pattern.substring(0,pattern.length()-prediction.length()-1)+")").length-1)-patternCount; //The lookahead allows the split to capture overlaps
-     	//subtract 1 from missed count if input string ends in the partial pattern (We can't miss a pattern we haven't fully seen yet)    	
-     	if (elem.endsWith(pattern.substring(0,pattern.length()-prediction.length()-1)+" ")) {
-     		patternMissedCount-=1;
-     	}
-     	
-     	float predictionStrength = 1-((float)patternMissedCount/(patternCount+patternMissedCount));
-    	//System.out.println(pattern+": "+patternCount+", "+pattern.substring(0,pattern.length()-prediction.length()-1)+": "+patternMissedCount+" = "+predictionStrength*100+"%");
-    	
-    	return predictionStrength;
-	}
-
-    
-    private static float determinePatternStrength(String elem, String pattern) {
-    	//Much more could probably go into this calculation. Currently it only compares the number of times the pattern occurs with and without the last element. Once could probably add many other comparisons to get a better value. 
-    	
-    	//count number of times pattern appears
-    	int patternCount = elem.split("(?="+pattern+")").length-1; //The lookahead allows the split to capture overlaps
-    	
-    	//count number of times pattern doesn't appear
-    	int idx = pattern.length();
-     	while (!pattern.substring(0,idx--).endsWith(" ")); //chop off the last element of the pattern 
-     	int patternMissedCount = (elem.split("(?="+pattern.substring(0,idx)+")").length-1)-patternCount; //The lookahead allows the split to capture overlaps
-     	//subtract 1 from missed count if input string ends in the partial pattern (We can't miss a pattern we haven't fully seen yet)    	
-     	if (elem.endsWith(pattern.substring(0,idx)+" ")) {
-     		patternMissedCount-=1;
-     	}
-     	
-     	float patternStrength = 1-((float)patternMissedCount/(patternCount+patternMissedCount));
-    	//System.out.println(pattern+": "+patternCount+", "+pattern.substring(0,idx)+": "+patternMissedCount+" = "+patternStrength*100+"%");
-    	
-    	return patternStrength;
-	}
-
 	public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
---
+++ src/motif/Node.java
@@ -0,0 +1,152 @@
+/**
+ * 
+ */
+package motif;
+
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Steve Kordell
+ *
+ */
+public class Node {
+
+	private String data;
+	public Node() {
+		this.data = "";
+	}
+	
+	/*
+	 * Takes a single frame, returns a string of predictions
+	 */
+	public String step(int frame) {
+		this.data+=frame+" ";
+		LinkedList<String> patterns;
+		
+		patterns = new LinkedList<String>();
+
+        System.out.println("------"+this.data+"------");
+        for (int i = 2; i < this.data.length()/2; i+=1){
+    	   String regex = "((\\d+\\s){"+i+"}).*\\1";
+	       Pattern p = Pattern.compile(regex);
+           Matcher matcher = p.matcher(this.data);
+           if (matcher.find()) {
+        	  patterns.add(matcher.group(1).trim());
+        	  int j = 0;
+        	  do {
+	        	  j = this.data.indexOf(' ',++j);
+        		  if (matcher.find(j) && !patterns.contains(matcher.group(1).trim())) {
+   		       		  patterns.add(matcher.group(1).trim());
+   		       	  }        	  
+        	  } while (j < this.data.length()-i); //todo: this can probably end sooner
+           } else {
+        	  //we won't find anymore patterns, leave loop early
+           	  break;
+           }
+       }
+  	   //we're done, print all the patterns we found
+ 	   int index = 0;
+ 	   for(String pattern : patterns){
+ 		   System.out.println("Pattern "+(index++)+": "+pattern);
+ 	   }
+ 	   
+    	//make a prediction as to what might come next	 
+    	String regex;
+    	Pattern p;
+    	Matcher matcher;
+    	LinkedList<String> predictions = new LinkedList<String>();
+    	LinkedList<Float> predictionStrengths = new LinkedList<Float>();
+    	for(String pattern : patterns) {
+    		regex = "\\s(";
+    		int i = 0;
+    		while ( (i = pattern.indexOf(' ',++i)) != -1) {
+    			regex+=pattern.substring(0,i).trim()+"|";
+    		}
+    		if (regex.endsWith("|")) {
+    			regex = regex.substring(0,regex.length()-1);
+    		}
+    		regex += ")\\s$";
+    		p = Pattern.compile(regex);        	        		
+    		matcher = p.matcher(this.data);
+    		if (matcher.find()) {
+    			//This pattern matches some part of the end of the input
+
+    			//chop off the portion of the pattern that intersects with the end of the input, leaving just the prediction
+    			int idx = pattern.length();
+             	while (!this.data.endsWith(pattern.substring(0, idx--)));
+             	String prediction = pattern.substring(idx + 1);
+             	
+             	Float predictionStrength = determinePredictionStrength(this.data, pattern, prediction);
+             	
+             	if (!predictions.contains(prediction)) { //remove repeated predictions
+          	   		predictions.add(prediction);
+          	   		predictionStrengths.add(predictionStrength);//The prediction strength is equal to the strength of the pattern the prediction came from
+             	} else {
+             		//compare strengths and keep the largest
+             		int predictionIndex = predictions.indexOf(prediction);
+             		if (predictionStrengths.get(predictionIndex) <  predictionStrength) {
+             			predictionStrengths.set(predictionIndex, predictionStrength);
+             		}
+             	}
+    		}
+    	}
+    	
+		float largestPredictionStrength = -1;
+		
+    	if (predictions.size() == 0) {
+    		System.out.println("No predicions");
+    	} else {
+    		int predictionIndex = 0;
+      	   	for(String prediction : predictions){
+      	   		System.out.println("Prediction: "+prediction+" = "+predictionStrengths.get(predictionIndex)*100+"%");
+      	   		if (predictionStrengths.get(predictionIndex) > largestPredictionStrength) {
+      	   			largestPredictionStrength = predictionStrengths.get(predictionIndex);
+      	   		}
+      	   		predictionIndex++;
+      	   	}
+    	}
+    	
+    	//Select a prediction (first, find prediction with highest accuracy. Break ties by selecting prediction with most this.dataents.
+      	if (predictions.size() > 0) {
+      		int predictionIndex = 0;
+      		int largestPredictionLength = -1;
+      		String selectedPrediction = null;
+      		for(String prediction : predictions){
+      			int predictionLength = countElements(prediction);
+      	   		if (predictionStrengths.get(predictionIndex) == largestPredictionStrength && predictionLength > largestPredictionLength) {
+      	   			selectedPrediction = prediction;
+      	   			largestPredictionLength = predictionLength;
+      	   		}
+      	   		predictionIndex++;
+      	   	}
+      		System.out.println("Selected Prediction: "+selectedPrediction+" = "+largestPredictionStrength*100 + "%");
+    	}
+
+		return "-1";
+	}
+	
+    
+    private static int countElements(String string) {
+		return string.split(" ").length;
+	}
+	
+	private static float determinePredictionStrength(String elem, String pattern, String prediction) {
+    	//count number of times pattern appears
+    	int patternCount = elem.split("(?="+pattern+")").length-1; //The lookahead allows the split to capture overlaps
+    	
+    	//count number of times prediction would have been wrong	 
+     	int patternMissedCount = (elem.split("(?="+pattern.substring(0,pattern.length()-prediction.length()-1)+")").length-1)-patternCount; //The lookahead allows the split to capture overlaps
+     	//subtract 1 from missed count if input string ends in the partial pattern (We can't miss a pattern we haven't fully seen yet)    	
+     	if (elem.endsWith(pattern.substring(0,pattern.length()-prediction.length()-1)+" ")) {
+     		patternMissedCount-=1;
+     	}
+     	
+     	float predictionStrength = 1-((float)patternMissedCount/(patternCount+patternMissedCount));
+    	//System.out.println(pattern+": "+patternCount+", "+pattern.substring(0,pattern.length()-prediction.length()-1)+": "+patternMissedCount+" = "+predictionStrength*100+"%");
+    	
+    	return predictionStrength;
+	}
+	
+}
